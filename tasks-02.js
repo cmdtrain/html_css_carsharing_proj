1515159ce74d9cf59d4104f3bcc76a8586239a2f
cmdtrain <72573030+cmdtrain@users.noreply.github.com>
2025-07-21 21:53:56 +0300

    Create tasks-02.js - codewars js code

---
+++ tasks-02.js
@@ -0,0 +1,10 @@
+//mykola dron
+// не зовсім зрозумів як це робити - але ось завдання з codewars
+//1
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+//2
+function stringToArray(str) {
+  return str.split(' ');
+}
